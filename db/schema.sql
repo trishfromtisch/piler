620300af543bb2f604dd662cd030652f4387cb2c
trishfromtisch <parlaws@gmail.com>
2014-10-10 16:04:13 -0400

    changes

--- db/schema.sql
+++ db/schema.sql
@@ -3,23 +3,27 @@ CREATE DATABASE piler;
 \c piler
 
 CREATE TABLE neighborhoods (
-	id serial primary key,
-	name varchar (255)
-	);
+id serial primary key,
+name varchar (255)
+);
 
 CREATE TABLE reports (
-	id serial primary key,
-	user_id integer,
-	location varchar(255),
-	subscribe boolean,
-	votes integer,
-	picture text
+id serial primary key,
+user_id integer,
+location varchar(255),
+subscribe boolean,
+votes integer,
+picture text,
+description text,
+created_at timestamp
 );
 
 CREATE TABLE comments (
-	id serial primary key,
-	user_id integer,
-	content text
+id serial primary key,
+user_id integer,
+report_id integer,
+content text,
+created_at timestamp
 );
 
 CREATE TABLE users (
