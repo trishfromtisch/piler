f357c22a58f1ea026b757669476a36599b641575
trishfromtisch <parlaws@gmail.com>
2014-10-10 14:24:14 -0400

    Pushing schema

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE piler;
+
+\c piler
+
+CREATE TABLE neighborhoods (
+	id serial primary key,
+	name varchar (255)
+	);
+
+CREATE TABLE reports (
+	id serial primary key,
+	user_id integer,
+	location varchar(255),
+	subscribe boolean,
+	votes integer,
+	picture text
+);
+
+CREATE TABLE comments (
+	id serial primary key,
+	user_id integer,
+	content text
+);
+
+CREATE TABLE users (
+id serial primary key,
+name varchar(255),
+email varchar(255),
+subscribe boolean,
+subscription_neighborhood_id integer,
+picture text,
+password varchar(15)
+);
+
