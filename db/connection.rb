62a86e97ac4279ece8822fb8a4c2f843b57bdb89
shotarokamegai <shotaro326@gmail.com>
2014-10-12 16:22:15 -0400

    trying resolve

--- db/connection.rb
+++ db/connection.rb
@@ -7,11 +7,7 @@ require_relative "../lib/user.rb"
 ActiveRecord::Base.establish_connection({
 	:adapter => "postgresql",
 	:host => "localhost",
-<<<<<<< HEAD
 	:username => "shotaro",
-=======
-	:username => "susrutcarpenter",
->>>>>>> 4d4871a0a71c6e1fbe0b70fe0c170dd5dadcb6fd
 	:database => "piler"
 	})
 
