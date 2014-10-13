51559a17631a548f239fec8ca22b266d27ced744
pateltejal9500 <pateltejal9500@gmail.com>
2014-10-13 13:55:25 -0400

    styling and close button

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -32,28 +32,22 @@ window.onload = function(){
     $('div.sidebar').append(template);
   })
 
-whichNeighborhood()
-
-function whichNeighborhood(){
-  //this is where they pick the neighborhood
   $.get("/neighborhoods", function(neighborhood){
-    neighborhoods = _.sortBy(neighborhood, function(neighborhoodObject) 
-    {return neighborhoodObject.name})
-    var innards = "<select name ='neighborhood' class='neighborhood'>"
-
+    neighborhoods = _.sortBy(neighborhood, function(neighborhoodObject) {return neighborhoodObject.name})
+    var innards = "<h2>Choose your neighborhood</h2><select name ='neighborhood' class='neighborhood'>"
+    var div = document.createElement('div');
+    $(div).attr('id', 'option');
     for (var i = 0; i < neighborhoods.length; i ++){
       innards += "<option value="+ neighborhoods[i].id+">" + neighborhoods[i].name + "</option>"
-    }
-
-    innards += "</select>"+"<button id='enter'>ENTER</button>"
-    $(".sidebar").html(innards)
+      }
+    innards += "</select>"+"<button class='btn btn-primary btn-sm' id='enter'>ENTER</button>"
+    $(div).html(innards);
+    $('div#piler').append(div);
     $("#enter").click(function(event){
       neighborhood_id = $("[name='neighborhood']").val()
-      RSS(neighborhood_id)
-    })
+    RSS(neighborhood_id)
   })
-}
-
+  })
 
 
 function RSS(neighborhood_id){
@@ -63,11 +57,12 @@ function RSS(neighborhood_id){
         alert("No reports for that area")
       } else {
         var innards = ""
-        for (var i = 0; i < 10; i++){
+        for (var i = 0; i < reports.length && i < 10; i++){
           innards += "<li>" + reports[i].created_at+"  <img src='"+reports[i].picture+"' width='50' height ='50'></li><button class='btn btn-primary btn-lg' data-toggle='modal' data-target='#"+reports[i].id+"'>MORE INFORMATION</button>"    
-           innards += "<div class='modal fade' id='"+reports[i].id+"' tabindex='-1' role='dialog' aria-labelledby='myModalLabel' aria-hidden='true'><div class='modal-dialog'><div class='modal-content'><div class='modal-header'><button type='button' class='close' data-dismiss='modal'><span aria-hidden='true'>&times;</span><span class='sr-only'>Close</span></button><h4 class='modal-title' id='myModalLabel'>"+reports[i].description+"<br>VOTES "+ reports[i].votes+" </h4></div><div class='modal-body'>"
+          innards += "<div class='modal fade' id='"+reports[i].id+"' tabindex='-1' role='dialog' aria-labelledby='myModalLabel' aria-hidden='true'><div class='modal-dialog'><div class='modal-content'><div class='modal-header'><button type='button' class='close' data-dismiss='modal'><span aria-hidden='true'>&times;</span><span class='sr-only'>Close</span></button><h4 class='modal-title' id='myModalLabel'>"+reports[i].description+"<br>VOTES "+ reports[i].votes+" </h4></div><div class='modal-body'>"
             innards += "</div><div class='modal-footer'><button type='button' class='btn btn-primary comment'>Add Comment</button><button type='button' class='btn btn-primary up'>UP VOTE</button><button type='button' class='btn btn-primary down'>DOWN VOTE</button><button type='button' class='btn btn-default close' data-dismiss='modal'>Close</button></div></div></div></div>"
             $(".sidebar").html(innards)
+            closeButton()
 
           $.get("/reports/"+reports[i].id+"/comments", function(comments){
             $(".modal-body").append("<h4>COMMENTS</h4>")
@@ -76,11 +71,11 @@ function RSS(neighborhood_id){
             }
           
           })
-         upVoting(reports[i].votes,reports[i].id)
-         downVoting(reports[i].votes, reports[i].id)
-         comment(reports[i])
+        upVoting(reports[i].votes,reports[i].id)
+        downVoting(reports[i].votes, reports[i].id)
+        comment(reports[i])
         }
-     
+    
       }
   })
 }
@@ -95,6 +90,8 @@ function upVoting(votes, id){
       data: {votes: newVotes},
       success: function(result){
         alert("Thanks for your Vote") 
+        $(".close").click()
+        window.location.reload()
       }
     })
   })
@@ -110,6 +107,8 @@ function downVoting(votes, id){
       data: {votes: newVotes},
       success: function(result){
         alert("Thanks for your Vote")
+        $(".close").click()
+        window.location.reload()
       }
     })
   })
@@ -119,20 +118,22 @@ function downVoting(votes, id){
 
 function comment(report){
   $(".comment").click(function(event){
-    $(".modal-body").html("<textarea name='comment' rows = 5 cols= 10 placeholder='Comment'></textarea><br><input name='email' placeholder='email'><input name='password' placeholder='password'><br><button class='commentEnter'>ENTER</button>")
+    $(".modal-body").html("<textarea name='comment' rows = 5 cols= 10 placeholder='Comment'></textarea><br><input name='email' placeholder='email' id='email'><input name='password' placeholder='password'><br><button class='commentEnter'>ENTER</button>")
     $(".commentEnter").click(function(event){
     email = $("[name='email']").val().toLowerCase()
     password = $("[name='password']").val().toLowerCase()
     $.get("/users", function(users){
       for (var i = 0; i < users.length; i ++){
           if (users[i].email.toLowerCase() == email && users[i].password.toLowerCase() == password){
-         comment = $("[name='comment']").val()
+        comment = $("[name='comment']").val()
           $.ajax({
             url:"/comments",
             type: 'POST',
             data:{report_id: report.id, content: comment, user_id: users[i].id},
             success: function(result){
             alert("Comment was Successful")
+            $(".close").click()
+            window.location.reload()
             }
           })
 
@@ -147,5 +148,15 @@ function comment(report){
   })
 }
 
+function closeButton(){
+   $(".close").click(function(event){
+
+
+   window.location.reload()
+   })
+
 }
 
+
+
+}
