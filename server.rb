9ae9e1ffb668380ef7e07a831ea02ebc9fc52236
pateltejal9500 <pateltejal9500@gmail.com>
2014-10-12 23:25:39 -0400

    server fixed, and modals for votes and comments

--- server.rb
+++ server.rb
@@ -1,17 +1,21 @@
 require "sinatra"
-require "sinatra/reloader"
 require "pry"
-require "active_support"
-require "json"
+require "rack"
+require 'rake'
+require "sinatra/activerecord"
+require "sinatra/contrib"
+
 require_relative "./db/connection.rb"
 require_relative "./lib/comment.rb"
 require_relative "./lib/neighborhood.rb"
 require_relative "./lib/report.rb"
 require_relative "./lib/user.rb"
+require "active_support"
 
 after do 
 	ActiveRecord::Base.connection.close
 end
+
 
 get "/" do
 	erb(:index)
@@ -40,10 +44,9 @@ end
 
 put "/reports/:id" do
 	content_type(:json)
-	report = Report.find(params["id"])
-	hash = JSON.parse(request.body.read)
-	report.update(hash)
-	hash.to_json
+	report = Report.find(params[:id])
+	report.update(report_params(params))
+	report.to_json
 end
 
 delete "/reports/:id" do
@@ -54,12 +57,18 @@ delete "/reports/:id" do
 end
 
 
-
 get "/reports/:id/comments" do
 	content_type(:json)
 	comments = Comment.where({report_id: params["id"]})
 	comments.to_json
 end
+
+post "/comments" do
+	content_type(:json)
+	comment = Comment.create(comment_params(params))
+	comment.to_json
+end
+
 
 #Users routes
 get "/users" do
@@ -117,7 +126,18 @@ end
 
 get "/neighborhoods/:id/reports" do
 	content_type(:json)
-	reports = Report.where( {neighborhood_id: params["id"]})
+	reports = Report.where({neighborhood_id: params["id"]})
 	reports.to_json
 end
+
+
+def report_params(params)
+  params.slice(*Report.column_names)
+end
+
+def comment_params(params)
+  params.slice(*Comment.column_names)
+end
+
+
 
