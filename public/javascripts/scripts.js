5bb64e82ccdccb92bcff5391156e31dab777a55b
pateltejal9500 <pateltejal9500@gmail.com>
2014-10-13 23:07:35 -0400

    heroku

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -45,7 +45,7 @@ function RSS(neighborhood_id){
          for (var i = 0; i < reports.length && i < 10; i++){
            innards += "<li>" + reports[i].created_at+"  <img src='"+reports[i].picture+"' width='50' height ='50'></li><button class='btn btn-primary btn-lg' data-toggle='modal' data-target='#"+reports[i].id+"'>MORE INFORMATION</button>"    
            innards += "<div class='modal fade' id='"+reports[i].id+"' tabindex='-1' role='dialog' aria-labelledby='myModalLabel' aria-hidden='true'><div class='modal-dialog'><div class='modal-content'><div class='modal-header'><button type='button' class='close' data-dismiss='modal'><span aria-hidden='true'>&times;</span><span class='sr-only'>Close</span></button><h4 class='modal-title' id='myModalLabel'>"+reports[i].description+"<br>VOTES "+ reports[i].votes+" </h4></div><div class='modal-body comment"+reports[i].id+"'>"
-             innards += "</div><div class='modal-footer'><button type='button' class='btn btn-primary comment'>Add Comment</button><button type='button' class='btn btn-primary up'>UP VOTE</button><button type='button' class='btn btn-primary down'>DOWN VOTE</button><button type='button' class='btn btn-default close' data-dismiss='modal'>Close</button></div></div></div></div>"
+             innards += "</div><div class='modal-footer'><button type='button' class='btn btn-primary comments"+reports[i].id+"'>Add Comment</button><button type='button' class='btn btn-primary up"+reports[i].id+"'>UP VOTE</button><button type='button' class='btn btn-primary down"+reports[i].id+"'>DOWN VOTE</button><button type='button' class='btn btn-default close' data-dismiss='modal'>Close</button></div></div></div></div>"
              $(".sidebar").html(innards)
             upVoting(reports[i].votes,reports[i].id)
             downVoting(reports[i].votes, reports[i].id)
@@ -82,7 +82,7 @@ function RSS(neighborhood_id){
 
 function upVoting(votes, id){
 
-  $(".up").click(function(event){
+  $(".up"+id).click(function(event){
     console.log(id)
     newVotes = votes + 1
     $.ajax({
@@ -100,7 +100,7 @@ function upVoting(votes, id){
 
 
 function downVoting(votes, id){
-  $(".down").click(function(event){
+  $(".down"+id).click(function(event){
     newVotes = votes - 1
     $.ajax({
       url:"/reports/"+id,
@@ -118,7 +118,7 @@ function downVoting(votes, id){
 
 
 function comment(report){
-  $(".comment").click(function(event){
+  $(".comments"+id).click(function(event){
     $(".modal-body").html("<textarea name='comment' rows = 5 cols= 10 placeholder='Comment'></textarea><br><input name='email' placeholder='email' id='email'><input name='password' placeholder='password'><br><button class='commentEnter'>ENTER</button>")
     $(".commentEnter").click(function(event){
     email = $("[name='email']").val().toLowerCase()
